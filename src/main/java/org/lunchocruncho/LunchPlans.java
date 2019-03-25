d60581b28f098059931ca1a6e5afb72e6b5da29c
Andrew Nicholson <Andrew.Nicholson@ithaka.org>
2019-03-25 09:26:14 -0400

    passing lunch with `public static LunchPlan setTodaysPlan(String day);`

--- src/main/java/org/lunchocruncho/LunchPlans.java
+++ src/main/java/org/lunchocruncho/LunchPlans.java
@@ -2,6 +2,17 @@ package org.lunchocruncho;
 
 public class LunchPlans {
     public static LunchPlan todaysPlan() {
-        return LunchPlan.NOT_WESTSIDE;
+        return setTodaysPlan("Today");
+    }
+
+    public static LunchPlan setTodaysPlan(String day) {
+        switch (day){
+            case "Today":
+                return LunchPlan.WESTSIDE;
+            case "Not_Today":
+                return LunchPlan.NOT_WESTSIDE;
+            default:
+                return null;
+        }
     }
 }
